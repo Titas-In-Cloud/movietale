f1809e511e868f30929295250fdb03843fb363e0
titas <t.petelis1@newcastle.ac.uk>
2021-12-11 02:40:46 +0000

    Created middleware

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,6 +2,14 @@ import axios from "axios";
 
 const API = axios.create({ baseURL: "http://localhost:5000" });
 
+API.interceptors.request.use((req) => {
+    if(localStorage.getItem("profile")) {
+        req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem("profile")).token}`;
+    }
+
+    return req;
+});
+
 export const fetchMoviesBySearch = (searchQuery) => API.get(`/movies/search?searchQuery=${searchQuery.search}`);
 export const fetchMovie = (id) => API.get(`/movies/${id}`);
 export const fetchMovies = () => API.get("/movies");
